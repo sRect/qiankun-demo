0f9c9129157939d917724ec03e0e632911ef0d3f
fangchaoqun <fcqun@sina.cn>
2021-03-26 13:35:08 +0800

    feat: style

--- qiankun_base/src/App.vue
+++ qiankun_base/src/App.vue
@@ -3,7 +3,7 @@
     <el-menu :router="true" mode="horizontal">
       <!-- 基座自己的路由 -->
       <el-menu-item index="/">base-home</el-menu-item>
-      <el-menu-item index="/about">base-about</el-menu-item>
+      <el-menu-item index="/baseAbout">base-about</el-menu-item>
       <!-- 引用其他子应用 -->
       <!-- <el-menu-item index="/vue">vue 应用</el-menu-item>
       <el-menu-item index="/react">react 应用</el-menu-item>
--- qiankun_base/src/router/index.js
+++ qiankun_base/src/router/index.js
@@ -6,18 +6,19 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/*',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
+    path: '/baseAbout',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/*',
+    name: 'Home',
+    component: Home
+  },
+  
 ]
 
 const router = new VueRouter({
